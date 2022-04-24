143f05f5a633f9ba350ab9304a2f118fde099766
ju <102438061+066444@users.noreply.github.com>
2022-04-24 16:25:05 -0300

    Add files via upload

---
+++ Pset1/MariaDB.sql
@@ -0,0 +1,243 @@
+create database uvv;
+
+show databases;
+
+use uvv;
+
+create user julia;
+
+grant all privileges on uvv to julia;
+
+CREATE TABLE departamento (
+                numero_departamento INT NOT NULL,
+                nome_departamento VARCHAR(15) NOT NULL,
+                cpf_supervisor CHAR(11) NOT NULL,
+                data_inicio_gerente DATE,
+                PRIMARY KEY (numero_departamento)
+);
+
+ALTER TABLE departamento COMMENT 'Tabela que armazena as informaçoẽs dos departamentos.';
+
+ALTER TABLE departamento MODIFY COLUMN numero_departamento INTEGER COMMENT 'Número do departamento. É a PK desta tabela.';
+
+ALTER TABLE departamento MODIFY COLUMN nome_departamento VARCHAR(15) COMMENT 'Nome do departamento. Deve ser único.';
+
+ALTER TABLE departamento MODIFY COLUMN cpf_supervisor CHAR(11) COMMENT 'CPF supervisor do departamento.  FK para a t.funcionários.';
+
+ALTER TABLE departamento MODIFY COLUMN data_inicio_gerente DATE COMMENT 'Data do início do gerente no departamento.';
+
+
+insert into departamento (numero_departamento, nome_departamento, cpf_supervisor, data_inicio_gerente)
+values ('5', 'Pesquisa', '33344555587', '1988-05-22'),
+
+insert into departamento (numero_departamento, nome_departamento, cpf_supervisor, data_inicio_gerente)
+values ('4', 'Administração','98765432168', '1995-01-01'),
+
+insert into departamento (numero_departamento, nome_departamento, cpf_supervisor, data_inicio_gerente)
+values ('1', 'Matriz', '88866555576', '1981-06-19');
+
+select * from departamento;
+
+
+CREATE TABLE localizacoes_departamento (
+                numero_departamento INT NOT NULL,
+                local VARCHAR(15) NOT NULL,
+                PRIMARY KEY (numero_departamento, local)
+);
+
+ALTER TABLE localizacoes_departamento COMMENT 'Tabela que armazena as possíveis localizações dos departamentos.';
+
+ALTER TABLE localizacoes_departamento MODIFY COLUMN numero_departamento INTEGER COMMENT 'Núm. do depart.PK desta tabela e FK para a t.departamento.';
+
+ALTER TABLE localizacoes_departamento MODIFY COLUMN local VARCHAR(15) COMMENT 'Localização do departamento. Faz parte da PK desta tabela.';
+
+insert into localizacoes_departamento
+(numero_departamento, local)
+values
+('1', 'São Paulo'),
+('4', 'Mauá'),
+('5', 'Santo André'),
+('5', 'Itu'),
+('5', 'São Paulo');
+
+select * from localizacoes_departamento;
+
+
+CREATE TABLE funcionario (
+                cpf CHAR(11) NOT NULL,
+                primeiro_nome VARCHAR(15) NOT NULL,
+                nome_meio CHAR(1),
+                ultimo_nome VARCHAR(15) NOT NULL,
+                data_nascimento DATE,
+                endereco VARCHAR(100),
+                sexo CHAR(1),
+                salario DECIMAL(10,2),
+                cpf_supervisor CHAR(11) NOT NULL,
+                numero_departamento INT NOT NULL,
+                PRIMARY KEY (cpf)
+);
+
+ALTER TABLE funcionario COMMENT 'Tabela que armazena as informações dos funcionários.';
+
+ALTER TABLE funcionario MODIFY COLUMN cpf CHAR(11) COMMENT 'CPF do funcionário. Será a PK da tabela.';
+
+ALTER TABLE funcionario MODIFY COLUMN primeiro_nome VARCHAR(15) COMMENT 'Primeiro nome do funcionário.';
+
+ALTER TABLE funcionario MODIFY COLUMN nome_meio CHAR(1) COMMENT 'Inicial do nome do meio.';
+
+ALTER TABLE funcionario MODIFY COLUMN ultimo_nome VARCHAR(15) COMMENT 'Sobrenome do funcionário.';
+
+ALTER TABLE funcionario MODIFY COLUMN endereco VARCHAR(100) COMMENT 'Endereço do funcionário.';
+
+ALTER TABLE funcionario MODIFY COLUMN sexo CHAR(1) COMMENT 'Sexo do funcionário.';
+
+ALTER TABLE funcionario MODIFY COLUMN salario DECIMAL(10, 2) COMMENT 'Salário do funcionário.';
+
+ALTER TABLE funcionario MODIFY COLUMN cpf_supervisor CHAR(11) COMMENT 'CPF superv. FK para a própria tabela (auto-relac.)';
+
+ALTER TABLE funcionario MODIFY COLUMN numero_departamento INTEGER COMMENT 'Número do departamento do funcionário.';
+
+insert into funcionario (cpf, primeiro_nome, nome_meio, ultimo_nome, data_nascimento, endereco, sexo, salario, cpf_supervisor, numero_departamento)
+values ('12345678966', 'João', 'B', 'Silva', '1965-01-09', 'Rua das Flores, 751, São Paulo, SP', 'M', '30000', '33344555587', '5');
+
+insert into funcionario (primeiro_nome, nome_meio, cpf, data_nascimento, endereco, sexo, salario, cpf_supervisor, numero_departamento)
+values ('33344555587', 'Fernando', 'T', 'Wong', '1955-12-08', 'Rua da Lapa, 34, São Paulo, SP', 'M', '40000', '88866555576', '5');
+
+insert into funcionario (primeiro_nome, nome_meio, cpf, data_nascimento, endereco, sexo, salario, cpf_supervisor, numero_departamento)
+values ('99988777767', 'Alice', 'J', 'Zelaya', '1968-01-19', 'Rua Souza Lima, 35, Curitiba, PR', 'F', '25000', '98765432168', '4');
+
+insert into funcionario (primeiro_nome, nome_meio, cpf, data_nascimento, endereco, sexo, salario, cpf_supervisor, numero_departamento)
+values ('98765432168', 'Jennifer', 'S', 'Souza', '1941-06-20', 'Av. Arthur de Lima, 54, Santo André, SP', 'F', '43000', '88866555576', '4');
+
+insert into funcionario (primeiro_nome, nome_meio, cpf, data_nascimento, endereco, sexo, salario, cpf_supervisor, numero_departamento)
+values ('66688444476', 'Ronaldo', 'K', 'Lima', '1962-09-15', 'Rua Rebouças, 65, Piracicaba, SP', 'M', '38000', '33344555587', '5');
+
+insert into funcionario (primeiro_nome, nome_meio, cpf, data_nascimento, endereco, sexo, salario, cpf_supervisor, numero_departamento)
+values ('45345345376', 'Joice', 'A', 'Leite', '1972-07-31', 'Av. Lucas Obes, 74, São Paulo, SP', 'F', '25000', 33344555587, '5');
+
+insert into funcionario (primeiro_nome, nome_meio, cpf, data_nascimento, endereco, sexo, salario, cpf_supervisor, numero_departamento)
+values ('98798798733', 'André', 'V', 'Pereira', '1963-03-29', 'Rua Timbira, 35, São Paulo, SP', 'M', '25000', 98765432168, '4');
+
+insert into funcionario (primeiro_nome, nome_meio, cpf, data_nascimento, endereco, sexo, salario, cpf_supervisor, numero_departamento)
+values ('88866555576', 'Jorge', 'E', 'Brito', '1937-11-10', 'Rua do Horto, 35, São Paulo, SP', 'M', '55000', '88866555576', '1');
+
+select * from funcionario;
+
+
+CREATE TABLE projeto (
+                numero_projeto INT NOT NULL,
+                nome_projeto VARCHAR(15) NOT NULL,
+                local_projeto VARCHAR(15),
+                numero_departamento INT NOT NULL,
+                PRIMARY KEY (numero_projeto)
+);
+
+ALTER TABLE projeto COMMENT 'Tabela que armazena as informações sobre os projetos dos departamentos.';
+
+ALTER TABLE projeto MODIFY COLUMN numero_projeto INTEGER COMMENT 'Número do projeto. É a PK desta tabela.';
+
+ALTER TABLE projeto MODIFY COLUMN nome_projeto VARCHAR(15) COMMENT 'Nome do projeto. Deve ser único.';
+
+ALTER TABLE projeto MODIFY COLUMN local_projeto VARCHAR(15) COMMENT 'Localização do projeto.';
+
+ALTER TABLE projeto MODIFY COLUMN numero_departamento INTEGER COMMENT 'Número do departamento. É uma FK para a tabela departamento.';
+
+
+CREATE UNIQUE INDEX projeto_idx
+ ON projeto
+ ( nome_projeto );
+
+insert into projeto (numero-projeto, nome_projeto, local_projeto, numero_departamento)
+values ('1', 'ProdutoX', 'Santo André', '5'),
+
+insert into projeto (numero-projeto, nome_projeto, local_projeto, numero_departamento)
+values ('2', 'ProdutoY', 'Itu', '5'),
+
+insert into projeto (numero-projeto, nome_projeto, local_projeto, numero_departamento)
+values ('3', 'ProdutoZ', 'São Paulo', '5'),
+
+insert into projeto (numero-projeto, nome_projeto, local_projeto, numero_departamento)
+values ('10', 'Informatização', 'São Paulo', '1'),
+
+insert into projeto (numero-projeto, nome_projeto, local_projeto, numero_departamento)
+values ('20', 'Reorganização', 'São Paulo', '1'),
+
+insert into projeto (numero-projeto, nome_projeto, local_projeto, numero_departamento)
+values ('30', 'Novosbenefícios',  'Mauá', '4');
+
+select * from projetos;
+
+
+CREATE TABLE dependente (
+                cpf_funcionario CHAR(11) NOT NULL,
+                nome_dependente VARCHAR(15) NOT NULL,
+                sexo CHAR(1),
+                data_nascimento DATE,
+                parentesco VARCHAR(15),
+                PRIMARY KEY (cpf_funcionario, nome_dependente)
+);
+
+ALTER TABLE dependente COMMENT 'Tabela que armazena as informações dos dependentes dos funcionários.';
+
+ALTER TABLE dependente MODIFY COLUMN cpf_funcionario CHAR(11) COMMENT 'CPF funcion. .PK desta tabela e uma FK para a t.funcionário';
+
+ALTER TABLE dependente MODIFY COLUMN nome_dependente VARCHAR(15) COMMENT 'Nome do dependente. Faz parte da PK desta tabela.';
+
+ALTER TABLE dependente MODIFY COLUMN sexo CHAR(1) COMMENT 'Sexo do dependente.';
+
+ALTER TABLE dependente MODIFY COLUMN data_nascimento DATE COMMENT 'Data de nascimento do dependente.';
+
+ALTER TABLE dependente MODIFY COLUMN parentesco VARCHAR(15) COMMENT 'Descrição do parentesco do dependente com o funcionário.';
+
+insert into dependente
+(cpf_funcionário, nome_dependente, sexo, data_nascimento, parentesco)
+values
+('33344555587', 'Alicia', 'F', '1986-04-05', 'Filha'),
+('33344555587', 'Tiago', 'M', '1983-10-25', 'Filho'),
+('33344555587', 'Janaína', 'F', '1958-05-03', 'Esposa'),
+('98765432168', 'Antonio', 'M', '1942-02-28', 'Marido'),
+('12345678966', 'Michael', 'M', '1988-01-04', 'Filho'),
+('12345678966', 'Alicia', 'F', '1988-12-30', 'Filha'),
+('12345678966', 'Elizabeth', 'F', '1967-05-05', 'Esposa');
+
+select * from dependente;
+
+
+CREATE TABLE trabalha_em (
+                cpf_funcionario CHAR(11) NOT NULL,
+                numero_projeto INT NOT NULL,
+                horas DECIMAL(3,1) NOT NULL,
+                PRIMARY KEY (cpf_funcionario, numero_projeto)
+);
+
+ALTER TABLE trabalha_em COMMENT 'Tabela para armazenar quais funcionários trabalham em quais projetos.';
+
+ALTER TABLE trabalha_em MODIFY COLUMN cpf_funcionario CHAR(11) COMMENT 'CPF funcionário.  PK desta tabela e FK para a t.funcionário.';
+
+ALTER TABLE trabalha_em MODIFY COLUMN numero_projeto INTEGER COMMENT 'Núm.projeto. PK desta tabela e FK para a t.projeto.';
+
+ALTER TABLE trabalha_em MODIFY COLUMN horas DECIMAL(3, 1) COMMENT 'Horas trabalhadas pelo funcionário neste projeto.';
+
+insert into trabalha_em
+(cpf_funcionario, numero_projeto, horas)
+values
+('12345678966', '1', '32.5'),
+('12345678966', '2', '7.5'),
+('66688444476', '3', '40.0'),
+('45345345376', '1', '20.0'),
+('45345345376', '2', '20.0'),
+('33344555587', '2', '10.0'),
+('33344555587', '3', '10.0'),
+('33344555587', '10', '10.0'),
+('33344555587', '20', '10.0'),
+('99988777767', '30', '30.0'),
+('99988777767', '10', '10.0'),
+('99988777767', '10', '35.0'),
+('88866555576', '20', '0');
+
+select * from trabalha_em;
+
+
+
+
+
